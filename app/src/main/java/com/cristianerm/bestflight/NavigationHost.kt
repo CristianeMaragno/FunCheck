979762e88e17dccf65ccdf4efa350806eb1ff62e
Cristiane Rodrigues Maragno <cristianerodriguesmaragno@gmail.com>
2020-08-17 16:22:36 -0300

    Add launcher icons

---
+++ app/src/main/java/com/cristianerm/bestflight/NavigationHost.kt
@@ -0,0 +1,11 @@
+package com.cristianerm.bestflight
+
+import androidx.fragment.app.Fragment
+
+interface NavigationHost {
+    /**
+     * Trigger a navigation to the specified fragment, optionally adding a transaction to the back
+     * stack to make this navigation reversible.
+     */
+    fun navigateTo(fragment: Fragment, addToBackstack: Boolean)
+}
