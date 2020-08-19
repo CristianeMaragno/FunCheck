515aede1d9edbbee713ff9da99c7f3d9c894d3d7
Cristiane Rodrigues Maragno <cristianerodriguesmaragno@gmail.com>
2020-08-19 17:58:31 -0300

    Layout of the Sign Up screen

--- app/src/main/java/com/cristianerm/bestflight/SignUpActivity.kt
+++ app/src/main/java/com/cristianerm/bestflight/SignUpActivity.kt
@@ -8,5 +8,12 @@ class SignUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
+
+        if (savedInstanceState == null) {
+            supportFragmentManager
+                .beginTransaction()
+                .add(R.id.container_sign_up, SignUpFragment())
+                .commit()
+        }
     }
 }
