f64541711166effe63c3f879cd3f3db9b5e41581
Cristiane Rodrigues Maragno <cristianerodriguesmaragno@gmail.com>
2020-08-31 18:03:01 -0300

    Try to use recycler view with the method used in Java. Nothing really works, but it's worth keep the the code in case is needed in the future

--- app/src/main/java/com/cristianerm/bestflight/DemoObjectFragment.kt
+++ app/src/main/java/com/cristianerm/bestflight/DemoObjectFragment.kt
@@ -1,11 +1,14 @@
 package com.cristianerm.bestflight
 
+import android.R
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_home_destinations.*
+
 
 class DemoObjectFragment : Fragment() {
 
@@ -17,8 +20,29 @@ class DemoObjectFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         arguments?.takeIf { it.containsKey(ARG_OBJECT) }?.apply {
+
+            var tab_page = getInt(ARG_OBJECT).toString()
             val textView: TextView = view.findViewById(R.id.textView_home_destinations)
-            textView.text = getInt(ARG_OBJECT).toString()
+
+            if (tab_page == "1"){
+                textView.text = getInt(ARG_OBJECT).toString()
+
+                val homeList: ArrayList<HomeItem> = ArrayList()
+                homeList.add(HomeItem("FLO X RIO", "14/07/2020", "20/07/2020"))
+                homeList.add(HomeItem("CUR X SP", "01/07/2020", "05/07/2020"))
+                homeList.add(HomeItem("RG X RIO", "04/07/2020", "28/07/2020"))
+
+                recyclerView_home.setHasFixedSize(true)
+                mLayoutManager = new LinearLayoutManager(this);
+                val mAdapter = DemoObjectAdapter(homeList);
+                recyclerView_home.setLayoutManager(mLayoutManager);
+                recyclerView_home.setAdapter(mAdapter);
+
+            }else{
+                textView.text = "Teste"
+            }
+
+
         }
     }
 }
---
+++ app/src/main/java/com/cristianerm/bestflight/HomeItem.kt
@@ -0,0 +1,3 @@
+package com.cristianerm.bestflight
+
+class HomeItem(val destination: String, val date_go: String, val date_back: String)
