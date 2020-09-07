eeaa8261f2556f6d2b708980dae7d2cf7d5b6f41
Cristiane Rodrigues Maragno <cristianerodriguesmaragno@gmail.com>
2020-09-07 16:09:56 -0300

    Changes some elements on the screen Add Destination and sends user to the Main Functionalities screen when there is a click on the close icon

--- app/src/main/java/com/cristianerm/bestflight/AddDestinationFragment.kt
+++ app/src/main/java/com/cristianerm/bestflight/AddDestinationFragment.kt
@@ -1,11 +1,15 @@
 package com.cristianerm.bestflight
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_add_destinations.*
+import kotlinx.android.synthetic.main.fragment_add_destinations.view.*
+import kotlinx.android.synthetic.main.fragment_sign_up.view.*
 
 class AddDestinationFragment: Fragment() {
     override fun onCreateView(
@@ -13,6 +17,10 @@ class AddDestinationFragment: Fragment() {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_add_destinations, container, false)
 
+        view.close_add_destination.setOnClickListener({
+            val intent = Intent(activity, MainFunctionalitiesActivity::class.java)
+            startActivity(intent)
+        })
         return view
     }
 }
