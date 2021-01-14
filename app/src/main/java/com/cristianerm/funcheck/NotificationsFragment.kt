77fb052e971d5e2de34d0f58ccdf324d1fafe239
Cristiane Rodrigues Maragno <cristianerodriguesmaragno@gmail.com>
2021-01-14 07:44:04 -0300

    Saves notification mode when save button is clicked

--- app/src/main/java/com/cristianerm/funcheck/NotificationsFragment.kt
+++ app/src/main/java/com/cristianerm/funcheck/NotificationsFragment.kt
@@ -9,34 +9,55 @@ import android.view.ViewGroup
 import android.widget.RadioButton
 import android.widget.RadioGroup
 import androidx.fragment.app.Fragment
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.FirebaseDatabase
 import kotlinx.android.synthetic.main.fragment_notifications.*
+import kotlinx.android.synthetic.main.fragment_notifications.view.*
 import kotlinx.android.synthetic.main.fragment_sign_up.*
 
 
 class NotificationsFragment : Fragment() {
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
 
-        radio_group_notifications.setOnCheckedChangeListener( RadioGroup.OnCheckedChangeListener { group, checkedId ->
-            val selectedRadioButtonId: Int = radio_group_notifications.checkedRadioButtonId
-            var radioButtonId = resources.getResourceEntryName(selectedRadioButtonId)
-            Log.v(ContentValues.TAG, radioButtonId)
+    var database = FirebaseDatabase.getInstance()
+    var myRef = database.getReference()
 
-            writeNotificationStatusOnDatabase()
-        })
-    }
+    private lateinit var auth: FirebaseAuth
+
+    var notificationStatus = ""
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_notifications, container, false)
 
+        auth = FirebaseAuth.getInstance()
+
+        view.save_button_notifications.setOnClickListener({
+            writeNotificationStatusOnDatabase()
+        })
+
         return view
     }
 
 
     private fun writeNotificationStatusOnDatabase(){
-        
+        val selectedRadioButtonId: Int = radio_group_notifications.checkedRadioButtonId
+        var radioButtonId = resources.getResourceEntryName(selectedRadioButtonId)
+
+        if (radioButtonId == "enable_all_radio_button_notifications"){
+            notificationStatus = "0"
+        }else if(radioButtonId == "enable_favorite_radio_button_notifications"){
+            notificationStatus = "1"
+        }else if(radioButtonId == "enable_marked_radio_button_notifications"){
+            notificationStatus = "2"
+        }else if(radioButtonId == "disable_notifications_radio_button_notifications"){
+            notificationStatus = "3"
+        }
+
+        val firebaseUser = this.auth.currentUser!!
+        val uid = firebaseUser.uid
+
+        myRef.child(uid).child("Notification").child("notification").setValue(notificationStatus)
     }
 
 }
